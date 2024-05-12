7db109a53360defbcc0d890b0ce1a4265c0d1666
Jonas <publik.froeller.jonas@gmx.at>
2024-05-12 16:20:17 +0200

    Initial Boilerplate

---
+++ src/app/clicker/clicker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clicker',
+  standalone: true,
+  imports: [],
+  templateUrl: './clicker.component.html',
+  styleUrl: './clicker.component.scss'
+})
+export class ClickerComponent {
+
+}
---
+++ src/app/types.ts
@@ -0,0 +1,8 @@
+type GameSave = {
+  name: string;
+  description: string;
+  score: number;
+  date: Date;
+  achievements: [{ name: string; description: string; receivedAt: Date }];
+  upgrades: [{ name: string; description: string; amount: number; createdAt: Date; updatedAt: Date; nextPrice: number; }];
+}
