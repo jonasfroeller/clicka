6bf6681a1c0a403eea55c25f660d3acf01359dd0
Jonas <publik.froeller.jonas@gmx.at>
2024-05-25 22:26:07 +0200

    fixed draggable test

--- src/app/draggable.directive.spec.ts
+++ src/app/draggable.directive.spec.ts
@@ -1,8 +1,21 @@
-import {DraggableDirective} from './draggable.directive';
+import { TestBed } from '@angular/core/testing';
+import { DraggableDirective } from './draggable.directive';
+import {ElementRef, PLATFORM_ID} from "@angular/core";
 
 describe('DraggableDirective', () => {
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: ElementRef, useValue: {nativeElement: {id: 'mock-id', offsetTop: 0, offsetLeft: 0}}},
+        {provide: PLATFORM_ID, useValue: 'browser'}
+      ]
+    });
+  });
+
   it('should create an instance', () => {
-    const directive = new DraggableDirective();
+    const elementRef = TestBed.inject(ElementRef);
+    const platformId = TestBed.inject(PLATFORM_ID);
+    const directive = new DraggableDirective(elementRef, platformId);
     expect(directive).toBeTruthy();
   });
 });
